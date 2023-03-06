1a414a3852c442f22acc59acff8cb2e636150ab2
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-03-06 20:24:22 +0100

    dynamiczne metadata

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -2,6 +2,7 @@
 
 import getUser from "@/lib/getUser";
 import getUserPosts from "@/lib/getUserPosts";
+import type { Metadata } from "next";
 import { Suspense } from "react";
 import UserPosts from "./components/UserPosts";
 
@@ -10,6 +11,16 @@ type Params = {
     userId: string;
   };
 };
+
+export async function generateMetadata({ params: { userId } }: Params):Promise<Metadata>{
+    const userData: Promise<User> = getUser(userId);
+    const user: User = await userData
+    return{
+        title: user.name,
+        description: `This is page of ${user.name}`
+    }
+}
+
 export default async function UserPage({ params: { userId } }: Params) {
   const userData: Promise<User> = getUser(userId);
   const userPostsData: Promise<Post[]> = getUserPosts(userId);
